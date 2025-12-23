35b7380936b1ead816f6cc8dd86e569826260063
BaboboiPR <teosebastiany@gmail.com>
2025-12-23 12:15:04 +0200

    function support

--- main.cpp
+++ main.cpp
@@ -4,14 +4,34 @@
 #include <fstream>
 using namespace std;
 
-string tokens[100];
+
 int form = 0;
 string word;
 int cnt = 0;
+
 string varName;
-string type_var;
+string type_var[100];
+string name_var[100];
 bool loop = false;
 int indent_level = 0;
+
+string type_function = "";
+string name_function = "";
+bool function[100];
+int function_count = 0;
+
+string function_line;
+string function_part;
+bool func_exist = false ;
+
+std::string str;
+int j = 0;
+int i = 0;
+int pos = 0;
+std::string tokens[100];
+
+std::istringstream iss(function_part);
+int pos_var = 0;
 
 void Tabs(ofstream& file) {
     for (int i = 0; i < indent_level; i++)
@@ -93,14 +113,28 @@ void Division(ofstream& file, const long long& m)
         if (stoi(tokens[m + 1]) != 0) {
             file << tokens[m - 1] << " / " << tokens[m + 1] << ";" << endl;
         }
+        if (stoi(tokens[m + 1]) == 0) {
+            cerr << "Error code 0: Division by 0!" << endl;
+        }
     }
 }
 //loop support
 void While(ofstream& file, const long long& m)
 {
-    if (tokens[m] == "while")
-    {
-        file << "while" << "(" << tokens[m + 1] << tokens[m + 2] << tokens[m + 3] << "){" << "\n";
+    if (tokens[m] == "while") {
+        if (tokens[m + 1] != "" and tokens[m + 2]!="")
+        {
+            file << "while" << "(" << tokens[m + 1] << tokens[m + 2] << tokens[m + 3] << "){" << "\n";
+        }
+        if (tokens[m + 1] != "" and tokens[m + 2]=="")
+        {
+            file << "while" << "(" << tokens[m + 1] << tokens[m + 2] << "){" << "\n";
+        }
+        if (tokens[m + 1] == "" and tokens[m + 2]=="")
+        {
+            file << "while" << "(" << tokens[m + 1]  << "){" << "\n";
+        }
+
         loop = true;
         indent_level++;
     }
@@ -119,7 +153,7 @@ void While(ofstream& file, const long long& m)
 
 
 //differation between functions/for/.. support
-void End(ofstream& file, const long long& m)
+void End(ofstream &file, const long long &m, std::istringstream &line)
 {
     if (tokens[m] == "end")
     {
@@ -128,13 +162,22 @@ void End(ofstream& file, const long long& m)
         loop = false;
         indent_level--;
     }
+    if (func_exist)
+    {
+        func_exist=false;
+        cerr << "Error code 2: ";
+    }
 }
 //library support
+bool imported[9];
 void import(ofstream& file, const long long& m)
 {
     if (tokens[m] == "import")
     {
-        if(tokens[m + 1]=="in_out") file << "#include <" << "iostream" << ">" << endl;
+        if(tokens[m + 1]=="in_out" and imported[0] == false) {
+            file << "#include <" << "iostream" << ">" << endl;
+            imported[0] = true;
+        }
         if (tokens[m + 1] == "array")
         {
             file << "#include <" << "array" << ">" << endl;
@@ -156,6 +199,9 @@ void import(ofstream& file, const long long& m)
         }
     }
 }
+
+
+
 //function main support
 void func_main(ofstream& file, const long long& m) {
     if (tokens[m] == "main" and tokens[m - 1] == "fn") {
@@ -163,59 +209,115 @@ void func_main(ofstream& file, const long long& m) {
         indent_level++;
     }
 }
+//another function support
+
+
+void func(ofstream& file,long long& m) {
+    if ((tokens[m] != "main" and tokens[m]!= "") and tokens[m - 1] == "fn" and function[function_count] == 0 and function[function_count+1] == false) {
+        file << "auto " << tokens[m] <<"() {" << endl;
+        name_function=tokens[m];
+    }
+    if (name_function == "main") {
+        cerr << "Error code 2: Name function become main when it shouldn't";
+    }
+}
+void func_calling(ofstream& file,long long& m) {
+    if ((tokens[m] != "main" and tokens[m]!= "") and tokens[m - 1] == "fn!" and function[function_count] == 0 and function[function_count+1] == false) {
+        file << tokens[m] <<"();" << endl;
+    }
+    if (name_function == "main") {
+        cerr << "Error code 2: Name function become main when it shouldn't";
+    }
+}
+//func_support
+
+int type_checking;
+void func_return(ofstream& file, const long long& m,ifstream& file1) {
+    if (tokens[m] == "return") {
+        type_function = tokens[m+2];
+    }
+    if (tokens[m] == "return" and name_function != "main") {
+        file << "return "<<tokens[m+1].substr(0,tokens[m+1].size()-1) << ";" << endl;
+        type_checking=check_loop(name_var,tokens[m+1].substr(0,tokens[m+1].size()-1));
+        if (type_checking==-1) {
+            cerr << "Error code 3: return error!";
+
+        }
+        if (tokens[m + 2] != type_var[type_checking]) {
+            if (tokens[m+2]=="Int" and type_var[type_checking]=="int"){}
+            else if (tokens[m+2]=="Float" and type_var[type_checking]=="float"){}
+            else if (tokens[m+2]=="Bool" and type_var[type_checking]=="bool"){}
+            else if (tokens[m+2]=="String" and type_var[type_checking]=="string"){}
+            else {
+                cerr << "Warning return type isn't the same as value";
+            }
+
+        }
+        // bool _var_type = check_loop(&type_var[type_checking],tokens[m+2]);
+        // if (_var_type==0) {
+        //     cerr << "Warning return type isn't the same as value";
+        // }
+    }
+}
 
 //Initialization support
 string intVariable(const long long& m, ofstream& file) {
 
-        if (tokens[m + 1].substr(0, 1) == "[" and tokens[m + 3] != "") {
-            file << tokens[m] << " " << tokens[m + 1];
-            file << " = " << tokens[m + 3] << ";" << endl;
-        }
-        if (tokens[m] == "int") {
-            file << "int ";
-            if (tokens[m + 2] == "=") {
-                file << tokens[m + 1] << ";" << endl;
+        if (tokens[m + 1] != "") {
+            if (tokens[m + 1].substr(0, 1) == "[" and tokens[m + 3] != "") {
+                file << tokens[m] << " " << tokens[m + 1];
+                file << " = " << tokens[m + 3] << ";" << endl;
             }
-
-            if (tokens[m + 2].substr(0, 1) == "[") {
-                file << tokens[m + 1];
-                file << tokens[m + 2] << ";" << endl;
-            }
-            type_var = "int";
-
-        }
-        else if (tokens[m] == "float") {
-            file << "float ";
-            if (tokens[m + 2] == "=") {
-                file << tokens[m + 1] << ";" << endl;
-
-            }
-            type_var = "float";
-        }
-        if (tokens[m] == "bool") {
-            file << "bool ";
-            if (tokens[m + 2] == "=") {
-                file << tokens[m + 1] << ";" << endl;
-            }
-            type_var = "bool";
-        }
-
-        else {
-            if (tokens[m] == "=" and tokens[m - 1].substr(0, 1) != "[") {
-                cerr << tokens[m + 2];
-                if (tokens[m + 2] == "" and tokens[m + 1] != "") {
-                    if (type_var == "int") file << tokens[m - 1] << " = " << stoi(tokens[m + 1]) << ";" << endl;//stoi = string to integer
-                    if (type_var == "float") file << tokens[m - 1] << " = " << stof(tokens[m + 1]) << ";" << endl;// string to float
-                    if (type_var == "bool") file << tokens[m-1] << " = " << stob(tokens[m + 1]) << ";" << endl;// string to bool
+            if (tokens[m] == "int") {
+                file << "int ";
+                if (tokens[m + 2] == "=") {
+                    file << tokens[m + 1] << ";" << endl;
+                    name_var[pos_var] = tokens[m + 1];
                 }
-                else {
-                    file << tokens[m - 1] << " = ";
+
+                if (tokens[m + 2].substr(0, 1) == "[") {
+                    file << tokens[m + 1];
+                    file << tokens[m + 2] << ";" << endl;
                 }
+                type_var[pos_var] = "int";
+
+            }
+            else if (tokens[m] == "float") {
+                file << "float ";
+                if (tokens[m + 2] == "=") {
+                    file << tokens[m + 1] << ";" << endl;
+                    name_var[pos_var] = tokens[m + 1];
+                }
+                type_var[pos_var] = "float";
+            }
+            if (tokens[m] == "bool") {
+                file << "bool ";
+                if (tokens[m + 2] == "=") {
+                    file << tokens[m + 1] << ";" << endl;
+                    name_var[pos_var] = tokens[m + 1];
+                }
+                type_var[pos_var] = "bool";
             }
 
+            else {
+                if (tokens[m] == "=" and tokens[m - 1].substr(0, 1) != "[") {
+
+                    cerr << tokens[m + 2];
+                    if (tokens[m + 2] == "" and tokens[m + 1] != "") {
+                        Tabs(file);
+                        if (type_var[pos_var] == "int") file << tokens[m - 1] << " = " << stoi(tokens[m + 1]) << ";" << endl;//stoi = string to integer
+                        if (type_var[pos_var] == "float") file << tokens[m - 1] << " = " << stof(tokens[m + 1]) << ";" << endl;// string to float
+                        if (type_var[pos_var] == "bool") file << tokens[m-1] << " = " << stob(tokens[m + 1]) << ";" << endl;// string to bool
+                    }
+                    else {
+                        file << tokens[m - 1] << " = ";
+                    }
+                }
+            }
         }
 
     return varName;
+    pos_var++;
 }
 
 void NewLine(ofstream& file, const long long& m) {
@@ -223,14 +325,11 @@ void NewLine(ofstream& file, const long long& m) {
         file << "cout << endl;" << "\n";
     }
 }
+
 void decode() {
     ifstream file("file.c--");
     ofstream file1("file1.cpp");
     file1 << "using namespace std;" << endl;
-    string str;
-    int j = 0;
-    int i = 0;
-    int pos = 0;
 
 
     while (getline(file, str)) {
@@ -260,21 +359,26 @@ void decode() {
         }
         Tabs(file1);
         for (long long m = 0; m < j; m++) {
-            import(file1,m);
-            func_main(file1,m);
-            intVariable(m, file1);
-            While(file1, m);
-            //For(file1, m);
-            print(m, file1);
-            input(m, file1);
-            Add(file1, m);
-            Minus(file1, m);
-            Multiplication(file1, m);
-            Division(file1, m);
-            NewLine(file1, m);
-            to_cpp(m, file1, file);
-            End(file1, m);
 
+                import(file1,m);
+                func_main(file1,m);
+                intVariable(m, file1);
+                While(file1, m);
+                //For(file1, m);
+                print(m, file1);
+                input(m, file1);
+                Add(file1, m);
+                Minus(file1, m);
+                Multiplication(file1, m);
+                Division(file1, m);
+                NewLine(file1, m);
+                to_cpp(m, file1, file);
+                func(file1, m);
+                func_return(file1, m,file);
+                func_calling(file1, m);
+
+
+            End(file1, m,iss);
         }
         i = 0;
         pos = 0;
--- utility functions.h
+++ utility functions.h
@@ -1,16 +1,50 @@
 #pragma once
 #include <iostream>
 #include <string>
-
-inline bool stob(const std::string &s) {
+#include <fstream>
+#include <sstream>
+inline int stob(const std::string &s) {
     if (s == "true") return true;
     else if (s == "false") return false;
     else if (s == "1") return true;
     else if (s == "0") return false;
     else
         {
-        std::cerr << "Error: variable isnt a bool returning false" << s << std::endl;
-        return false;
+        std::cerr << "Error code 1: variable isn't a bool, returning -1" << s << std::endl;
+        return -1;
     }
 }
 
+void reset_file(std::ifstream &file) {
+    file.clear();                 // clear eof/fail flags
+    file.seekg(0, std::ios::beg); // go back to start
+}
+
+
+int check_loop(std::string name[],std::string string_name) {
+    for (int i = 0; i < 99; i++) {
+        if (name[i] == string_name) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+std::string table [10][10]
+{
+    {"Int","int"},
+    {"Float","float"},
+    {"String","string"},
+    {"Bool","bool"}
+};
+bool check_var(std::string type_var,std::string token) {
+
+    for (int i = 0; i < 4; i++) {
+        if (table[i][0]==type_var && table[i][1]==token) {
+            return true;
+        }
+    }
+    std::cerr<<"Error code 3: Type of variable doesn't exist";
+    return false;
+
+}
